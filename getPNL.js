a9cf366a7a01d47499bab64a84a052c6f1fa547c
Aaron Iniguez <aaron.iniguez@vrtcal.com>
2019-03-22 14:14:48 -0700

    Fix: removed redundant tefs code

--- getPNL.js
+++ getPNL.js
@@ -6,9 +6,11 @@ var tefsCredentials = JSON.parse(rawdata).TEFS;
 var twitterCredentials = JSON.parse(rawdata).Twitter;
 var twitterCredentialsPromo = JSON.parse(rawdata).TwitterPromo;
 var facebookCredentials = JSON.parse(rawdata).Facebook
-tefs.tefs(tefsCredentials.username, tefsCredentials.password).then(data => facebook.postOnFacebook(facebookCredentials.username, facebookCredentials.password, data, "dailyPNL.png")).catch(function(error){
-	console.log(error);
+tefs.tefs(tefsCredentials.username, tefsCredentials.password).then(data => 
+{
+	twitter.postOnTwitter(twitterCredentials.username, twitterCredentials.password, data + "\n\n #tefs #TradeNet #suretrader", "dailyPNL.png");
+	facebook.postOnFacebook(facebookCredentials.username, facebookCredentials.password, data, "dailyPNL.png");
+}).catch(function(error){
+	console.log(error);	
 })
-tefs.tefs(tefsCredentials.username, tefsCredentials.password).then(data => twitter.postOnTwitter(twitterCredentials.username, twitterCredentials.password, data + "\n\n #tefs #TradeNet #suretrader", "dailyPNL.png")).catch(function(error){
-	console.log(error);
-});
+
