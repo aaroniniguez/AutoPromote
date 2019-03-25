98d2e389ee0d8446a2c32c36f3d23c97adef5042
Aaron Iniguez <aaron.iniguez@vrtcal.com>
2019-03-24 23:44:49 -0700

    Feature: implemented auto follow a random person

--- promo.js
+++ promo.js
@@ -9,6 +9,6 @@ var ampm = today.getHours() >= 12 ? "PM" : "AM";
 var minutes = today.getMinutes();
 minutes = minutes < 10 ? "0"+minutes : minutes;
 var message = "It's " + hours + ":" + today.getMinutes() + " " + ampm + "!! That means its promotion time :D \n\n";
-twitter.postOnTwitter(twitterCredentialsPromo.username, twitterCredentialsPromo.password, message + twitterCredentialsPromo.post).catch(function(error){
+twitter.postOnTwitter(twitterCredentialsPromo.username, twitterCredentialsPromo.password, message + twitterCredentialsPromo.post, uploadFile = false, randomFollow = true).catch(function(error){
 	console.log(error);
 });
