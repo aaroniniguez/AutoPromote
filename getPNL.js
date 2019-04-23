2e4a51dbb21e677d853fd97a5f56f6afe13ef021
Aaron Iniguez <aaron.iniguez@vrtcal.com>
2019-04-22 17:24:59 -0700

    Fix: converted mysql callbacks to use promises

--- getPNL.js
+++ getPNL.js
@@ -7,8 +7,9 @@ var twitterCredentials = JSON.parse(rawdata).Twitter;
 var twitterCredentialsPromo = JSON.parse(rawdata).TwitterPromo;
 var facebookCredentials = JSON.parse(rawdata).Facebook
 
+//console.log(tefs.getWinStreak());
 tefs.getTefs(tefsCredentials.username, tefsCredentials.password).then(data => {
-	//twitter.postOnTwitter(twitterCredentials.username, twitterCredentials.password, data + "\n\n #Tefs #TradeNet #SuretraderKiller", "dailyPNL.png");
+	twitter.postOnTwitter(twitterCredentials.username, twitterCredentials.password, data + "\n\n #Tefs #TradeNet #SuretraderKiller", "dailyPNL.png");
 	facebook.postOnFacebook(facebookCredentials.username, facebookCredentials.password, data, "dailyPNL.png");
 }).catch(function(error){
 	console.log(error);	
