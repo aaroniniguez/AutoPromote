d2ec8afe5395eda3c71e6f1a8eaf2abe4e32270e
Aaron Iniguez <aaron.iniguez@vrtcal.com>
2019-04-25 17:14:50 -0700

    Feature: created the database wrapper as a module exported class

---
+++ node_modules/Database.js
@@ -0,0 +1,27 @@
+const mysql = require("mysql")
+class Database {
+	constructor(){
+		this.connection = mysql.createConnection({
+			host: "localhost",
+			user: "root",
+			database: "stock"
+		});
+	}
+	query(sql, args){
+		return new Promise((resolve, reject) => {
+			this.connection.query(sql, args, (err, rows)=> {
+				if(err) return reject(err);
+				resolve(rows);
+			});
+		});
+	}
+	close(){
+		return new Promise((resolve, reject) => {
+			this.connection.end( err => {
+				if(err) return reject(err)
+				resolve();
+			});
+		});
+	}
+}
+module.exports = new Database()
