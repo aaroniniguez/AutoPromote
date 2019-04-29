1f1329615e603438399d1b6a89829007d829fa28
Aaron Iniguez <aaron.iniguez@vrtcal.com>
2019-04-29 16:50:53 -0700

    [Fix]: added twitter verify login

--- promo.js
+++ promo.js
@@ -9,6 +9,12 @@ var ampm = today.getHours() >= 12 ? "PM" : "AM";
 var minutes = today.getMinutes();
 minutes = minutes < 10 ? "0"+minutes : minutes;
 var message = "It's " + hours + ":" + today.getMinutes() + " " + ampm + "!! That means its promotion time :D \n\n";
-twitter.postOnTwitter(twitterCredentialsPromo.username, twitterCredentialsPromo.password, message + twitterCredentialsPromo.post, uploadFile = false, randomFollow = true).catch(function(error){
+//twitter.verifyLoginInfo(twitterCredentialsPromo.username, twitterCredentialsPromo.password).catch(function(error)
+//{
+//	console.log(error);
+//});
+twitter.postOnTwitter(twitterCredentialsPromo.username, twitterCredentialsPromo.password, message + twitterCredentialsPromo.post, uploadFile = false, randomFollow = false).catch(function(error){
 	console.log(error);
+	console.log(error.name);
+	process.exit();
 });
