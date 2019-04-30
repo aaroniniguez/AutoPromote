c552630c96de58b9efcaace558435ca1e0b9d1ad
Aaron Iniguez <aaron.iniguez@vrtcal.com>
2019-04-30 11:30:38 -0700

    [Feature] added code for testing verifyLogin

--- twit.js
+++ twit.js
@@ -2,8 +2,8 @@ var password = process.argv[3];
 var username = process.argv[2];
 var tweet = process.argv[4];
 //do data validation here;
-username = "twitmore1";
-password = "Impossible123!!";
+username = "jhonboger";
+password = "Impossible123!!2";
 tweet = "oh nope";
 console.log(username);
 console.log(password);
@@ -13,8 +13,12 @@ if(!password || !username || !tweet){
     process.exit();
 }
 const twitter = require("twitter");
-twitter.postOnTwitter(username, password, tweet, uploadFile = false, randomFollow = false).catch(function(error){
+//twitter.postOnTwitter(username, password, tweet, uploadFile = false, randomFollow = false).catch(function(error){
+ //   console.log(error);
+  //  console.log(error.name);
+   // process.exit();
+//});
+twitter.verifyLoginInfo(username, password).catch((error) => {
     console.log(error);
-    console.log(error.name);
     process.exit();
 });
