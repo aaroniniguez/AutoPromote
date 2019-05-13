ec3cec8b1b55cb4a7fb09e79b89007260e903227
Aaron Iniguez <aaron.iniguez@vrtcal.com>
2019-05-13 09:52:54 -0700

    [Feature] promote or write a stock message

--- promo.js
+++ promo.js
@@ -1,4 +1,6 @@
 const twitter = require("twitter");
+const database = require("Database");
+let DB =new database("localhost", "root", "stock");
 let rawdata = require('fs').readFileSync('secret.json');  
 var twitterCredentials = JSON.parse(rawdata).Twitter;
 var twitterCredentialsPromo = JSON.parse(rawdata).TwitterPromo;
@@ -6,15 +8,23 @@ var twitterCredentialsPromo = JSON.parse(rawdata).TwitterPromo;
 var today = new Date();
 var hours = ((today.getHours() + 11) % 12 + 1);
 var ampm = today.getHours() >= 12 ? "PM" : "AM";
-var minutes = today.getMinutes();
-minutes = minutes < 10 ? "0"+minutes : minutes;
-var message = "It's " + hours + ":" + today.getMinutes() + " " + ampm + "!! That means its promotion time :D \n\n";
+const promote = process.argv[2];
+//query db here to get the specific message
+if(promote){
+	var message = twitterCredentialsPromo.message
+}
 //twitter.verifyLoginInfo(twitterCredentialsPromo.username, twitterCredentialsPromo.password).catch(function(error)
 //{
 //	console.log(error);
 //});
-twitter.postOnTwitter(twitterCredentialsPromo.username, twitterCredentialsPromo.password, message + twitterCredentialsPromo.post, uploadFile = false, randomFollow = false).catch(function(error){
+DB.moveFirstRowToEnd().then(()=>{DB.query("select * from stockQuotes").then((data)=>
+	{
+		var message = data[0].quote;  
+		twitter.postOnTwitter(twitterCredentialsPromo.username, twitterCredentialsPromo.password, message, uploadFile = false, randomFollow = true);
+	}
+).then(()=>{DB.close();}).catch(function(error){
 	console.log(error);
 	console.log(error.name);
 	process.exit();
+})
 });
