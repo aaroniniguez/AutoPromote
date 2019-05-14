e3174ac0dd34e3e073f7db2bbc188d41d89238c7
Aaron Iniguez <aaron.iniguez@vrtcal.com>
2019-05-14 10:28:20 -0700

    [Fix] fixed promote message var scope

--- promo.js
+++ promo.js
@@ -10,16 +10,16 @@ var hours = ((today.getHours() + 11) % 12 + 1);
 var ampm = today.getHours() >= 12 ? "PM" : "AM";
 const promote = process.argv[2];
 //query db here to get the specific message
-if(promote){
+if(promote)
 	var message = twitterCredentialsPromo.message
-}
 //twitter.verifyLoginInfo(twitterCredentialsPromo.username, twitterCredentialsPromo.password).catch(function(error)
 //{
 //	console.log(error);
 //});
 DB.moveFirstRowToEnd().then(()=>{DB.query("select * from stockQuotes").then((data)=>
 	{
-		var message = data[0].quote;  
+		if(!promote)
+			message = data[0].quote;  
 		twitter.postOnTwitter(twitterCredentialsPromo.username, twitterCredentialsPromo.password, message, uploadFile = false, randomFollow = true);
 	}
 ).then(()=>{DB.close();}).catch(function(error){
