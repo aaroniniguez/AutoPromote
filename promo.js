39fcf4f0de2f8eb4ff35991c80e89b15a519f7e9
Aaron Iniguez <aaron.iniguez@vrtcal.com>
2019-09-27 11:39:12 -0700

    [Refactor] removed old comments

--- promo.js
+++ promo.js
@@ -1,15 +1,6 @@
 var twitter = require("./node_modules/Twitter");
 const database = require("Database");
 let DB =new database("localhost", "root", "stock");
-//var mysql = require("mysql")
-//var myNew = mysql.createConnection({
-	//host: "localhost",
-	//user: "root",
-	//database: "stock"
-//});
-//myNew.connect()
-//console.log(myNew);
-//process.exit();
 let rawdata = require('fs').readFileSync('secret.json');  
 var twitterCredentialsPromo = JSON.parse(rawdata).TwitterPromo;
 var robinHoodCredentialsPromo = JSON.parse(rawdata).RobinHoodPromo;
@@ -17,22 +8,6 @@ var robinHoodCredentialsPromo = JSON.parse(rawdata).RobinHoodPromo;
 var today = new Date();
 var hours = ((today.getHours() + 11) % 12 + 1);
 var ampm = today.getHours() >= 12 ? "PM" : "AM";
-//const promote = process.argv[2];
-//default message:
-//if(promote == "tradenet"){
-	//var tradenetMessage = twitterCredentialsPromo.message;
-//}
-//else if(promote == "robinhood") {
-	//var robinHoodMessage = robinHoodCredentialsPromo.message;
-//}
-//else if(promote == "promote") {
-	//var tradenetMessage = twitterCredentialsPromo.message
-	//var robinHoodMessage = robinHoodCredentialsPromo.message
-//}
-
-//twitter.verifyLoginInfo(twitterCredentialsPromo.username, twitterCredentialsPromo.password).catch(function(error) {
-	//console.log(error);
-//});
 function promote() {
 	twitter.postOnTwitter(twitterCredentialsPromo.username, twitterCredentialsPromo.password, twitterCredentialsPromo.message, uploadFile = false, randomFollow = true);
 	twitter.postOnTwitter(robinHoodCredentialsPromo.username, robinHoodCredentialsPromo.password, robinHoodCredentialsPromo.message, uploadFile = false, randomFollow = true);
@@ -57,5 +32,4 @@ function tweetQuote() {
 }
 tweetQuote();
 //promote();
-
 //sudo mysql stop
