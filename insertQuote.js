b6103cc42bf5129558d59e69a8d9b9adfb4bce2b
Aaron Iniguez <aaron.iniguez@vrtcal.com>
2019-12-30 18:42:29 -0800

    [Fix] remove whitespace before and after the quote

--- insertQuote.js
+++ insertQuote.js
@@ -1,6 +1,6 @@
 const database = require("./lib/Database.js");
 let DB = new database()
-var quote = process.argv[2];
+var quote = process.argv[2].trim();
 DB.query(`insert into stockQuotes (quote) values("${quote}")`)
     .then(() => DB.query(`select count(*) from stockQuotes`).then(data => console.log(data)))
     .catch((e)=> {
