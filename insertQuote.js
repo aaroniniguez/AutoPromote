fc3d079321520eaf65ca5a324d16b98e66ba5607
Aaron Iniguez <aaron.iniguez@vrtcal.com>
2020-01-24 17:23:56 -0800

    [Fix] no need to close DB query manually anymore

--- insertQuote.js
+++ insertQuote.js
@@ -2,9 +2,6 @@ const database = require("./lib/Database.js");
 let DB = new database()
 var quote = process.argv[2].trim();
 DB.query(`insert into stockQuotes (quote) values("${quote}")`)
-    .then(() => DB.query(`select count(*) from stockQuotes`).then(data => console.log(data)))
     .catch((e)=> {
         console.log("Duplicate Entries Not Allowed!");
-        //console.log(e);
-    })
-    .finally(() => DB.close().catch((e)=>{console.log("couldnt close properly");}));
+	})
