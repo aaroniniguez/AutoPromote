822338c8805ed7c887a69b6eb60d55401e27f475
aaroniniguez1@gmail.com <aaroniniguez1@gmail.com>
2020-05-03 16:08:53 -0700

    [Fix] added typescript to follow page

--- src/lib/PageObjects/FollowPage.ts
+++ src/lib/PageObjects/FollowPage.ts
@@ -1,9 +1,11 @@
 class FollowPage {
+	url: string;
+	version: string;
 	/**
 	 * 
 	 * @param {String} version either mobile or desktop
 	 */
-	constructor(version) {
+	constructor(version?: string) {
 		this.version = version;
 		this.url = "https://twitter.com/i/connect_people"
 	}
