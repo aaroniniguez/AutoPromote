5498e22f4692a59dafe387e8db639b81c58c6d99
aaroniniguez1@gmail.com <aaroniniguez1@gmail.com>
2020-07-18 13:25:06 -0700

    refactor promotion code into one generic promote function that can be applied to all promotions

--- src/promote.ts
+++ src/promote.ts
@@ -3,45 +3,12 @@ import twitter from "./lib/Twitter";
 import TwitterAccountsDAO from "./lib/DAO/TwitterAccountsDAO";
 import PromotionsDAO from "./lib/DAO/PromotionsDAO";
 
-async function tweetAirbnb() {
+export async function promote(promotion: string) {
 	let TwitterAccountDAO = new TwitterAccountsDAO();
-	let twitterAccountInfo = await TwitterAccountDAO.getTwitterAccountByType("airbnb");
+	let twitterAccountInfo = await TwitterAccountDAO.getTwitterAccountByType(promotion);
 	TwitterAccountDAO.cleanup()
 	let promotionsDAO = new PromotionsDAO()
-	let promotionInfo = await promotionsDAO.getRandomTweet("airbnb");
-	await promotionsDAO.cleanup()
-	let twitterAccount = new twitter(twitterAccountInfo.username, twitterAccountInfo.password)
-	twitterAccount
-		.tweet(promotionInfo.post, promotionInfo.image)
-		.then(() => twitterAccount.update())
-		.catch((e) => console.trace(e))
-		.finally(() => {
-			twitterAccount.close();
-		})
-}
-async function tweetPostmates() {
-	let TwitterAccountDAO = new TwitterAccountsDAO();
-	let promotionsDAO = new PromotionsDAO()
-	let promotionInfo = await promotionsDAO.getRandomTweet("postmates");
-	await promotionsDAO.cleanup()
-	let twitterAccountInfo = await TwitterAccountDAO.getTwitterAccountByType("postmates");
-	TwitterAccountDAO.cleanup()
-	let twitterAccount = new twitter(twitterAccountInfo.username, twitterAccountInfo.password)
-	twitterAccount
-		.tweet(promotionInfo.post, promotionInfo.image)
-		.then(() => twitterAccount.update())
-		.catch((e) => console.trace(e))
-		.finally(() => {
-			twitterAccount.close();
-		})
-}
-
-async function tweetTradenet() {
-	let TwitterAccountDAO = new TwitterAccountsDAO();
-	let twitterAccountInfo = await TwitterAccountDAO.getTwitterAccountByType("tradenet");
-	TwitterAccountDAO.cleanup()
-	let promotionsDAO = new PromotionsDAO()
-	let promotionInfo = await promotionsDAO.getRandomTweet("tradenet");
+	let promotionInfo = await promotionsDAO.getRandomTweet(promotion);
 	promotionsDAO.cleanup()
 	let twitterAccount = new twitter(twitterAccountInfo.username, twitterAccountInfo.password)
 	twitterAccount
@@ -51,18 +18,5 @@ async function tweetTradenet() {
 		.finally(() => twitterAccount.close())
 }
 
-export function promote(promotion: string) {
-	switch(promotion) {
-		case "airbnb":
-			tweetAirbnb()
-			break;
-		case "tradenet": 
-			tweetTradenet();
-			break;
-		case "postmates": 
-			tweetPostmates()
-			break;
-		default:
-			console.log("Invalid Promotion")
-	}
-}
+// testing:
+promote(process.argv[2])
