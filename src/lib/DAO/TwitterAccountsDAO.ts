963e8c4b4a71a13426363f9b42c860758f14173b
aaroniniguez1@gmail.com <aaroniniguez1@gmail.com>
2020-08-10 19:25:31 -0700

    change twitterAccounts table name to twitter_accounts

--- src/lib/DAO/TwitterAccountsDAO.ts
+++ src/lib/DAO/TwitterAccountsDAO.ts
@@ -2,54 +2,56 @@ import readableTimestamp from "../../utils/readable-timestamp";
 import BaseDao from "./BaseDAO";
 
 export default class TwitterAccountsDAO extends BaseDao {
+	private tableName: string;
 	constructor(public username?: string) {
 		super()
 		this.username = username;
+		this.tableName = 'twitter_accounts';
 	}
 
 	async getTwitterAccount(username: string) {
-		return (await this.DB.query(`SELECT * FROM twitterAccounts WHERE username="${username}"`))[0];
+		return (await this.DB.query(`SELECT * FROM ${this.tableName} WHERE username="${username}"`))[0];
 	}
 
 	async getAllTwitterAccounts() {
-		return await this.DB.query("SELECT * FROM twitterAccounts");
+		return await this.DB.query(`SELECT * FROM ${this.tableName}`);
 	}
 
 	async getTwitterAccountsByType(type: string) {
-		return await this.DB.query(`SELECT * FROM twitterAccounts WHERE type="${type}" and suspended=0`);
+		return await this.DB.query(`SELECT * FROM ${this.tableName} WHERE type="${type}" and suspended=0`);
 	}
 
 	//TODO: the func above will get deleted soon in a refactor... use this one
 	async getTwitterAccountByType(promo: string) {
-		return (await this.DB.query(`SELECT * FROM twitterAccounts WHERE type="${promo}" AND suspended=0 ORDER BY last_tweeted ASC`))[0];
+		return (await this.DB.query(`SELECT * FROM ${this.tableName} WHERE type="${promo}" AND suspended=0 ORDER BY last_tweeted ASC`))[0];
 	}
 
 	async setSuspended(username: string) : Promise<void> {
-		await this.DB.query(`UPDATE twitterAccounts SET suspended = true WHERE username='${username}'`)
+		await this.DB.query(`UPDATE ${this.tableName} SET suspended = true WHERE username='${username}'`)
 	}
 
 	async getSuspended(username: string) : Promise<number>{
-		return (await this.DB.query(`SELECT suspended FROM twitterAccounts WHERE username='${username}'`))[0]["suspended"];
+		return (await this.DB.query(`SELECT suspended FROM ${this.tableName} WHERE username='${username}'`))[0]["suspended"];
 	}
 
 	async addNewAccount(username: string, password: string, email: string, phone: string) {
 		//TODO: temp pass in 1 for owner
 		let query = `
-			INSERT INTO twitterAccounts (username, password, email, phone, owner)
+			INSERT INTO ${this.tableName} (username, password, email, phone, owner)
 			VALUES ("${username}", "${password}", "${email}", "${phone}", 1)
 		`;
 		return await this.DB.query(query);
 	}
 	async getNumberFollowing() : Promise<number>{
-		return (await this.DB.query(`SELECT following FROM twitterAccounts where username='${this.username}'`))[0]["following"];
+		return (await this.DB.query(`SELECT following FROM ${this.tableName} where username='${this.username}'`))[0]["following"];
 	}
 
 	async getAccountID(): Promise<number> {
-		return (await this.DB.query(`SELECT id FROM twitterAccounts WHERE username='${this.username}'`))[0]["id"];
+		return (await this.DB.query(`SELECT id FROM ${this.tableName} WHERE username='${this.username}'`))[0]["id"];
 	}
 
 	async updateFollowing(numFollowing: number) {
-		await this.DB.query(`UPDATE twitterAccounts set following = '${numFollowing}' WHERE username = '${this.username}'`);
+		await this.DB.query(`UPDATE ${this.tableName} set following = '${numFollowing}' WHERE username = '${this.username}'`);
 	}
 	
 	async updateFollowers(numFollowers: number) {
@@ -59,7 +61,7 @@ export default class TwitterAccountsDAO extends BaseDao {
 	}
 
 	async updateLastTweeted() {
-		let query = `UPDATE twitterAccounts SET last_tweeted = "${readableTimestamp()}" WHERE username = "${this.username}";`;
+		let query = `UPDATE ${this.tableName} SET last_tweeted = "${readableTimestamp()}" WHERE username = "${this.username}";`;
 		await this.DB.query(query);
 	}
 }
