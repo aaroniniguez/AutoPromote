fc7405c5d12cefada411572cd516913590fd93df
aaroniniguez1@gmail.com <aaroniniguez1@gmail.com>
2020-12-23 10:49:24 -0800

    ran linter

--- src/promote.ts
+++ src/promote.ts
@@ -2,16 +2,15 @@ require("dotenv").config();
 import { TwitterPromoter } from "./lib/Twitter";
 import TwitterAccountsDAO from "./lib/DAO/TwitterAccountsDAO";
 import PromotionsDAO from "./lib/DAO/PromotionsDAO";
-import { Logger } from "./lib/Logger";
 
 export async function promote(promotion: string) {
-	let TwitterAccountDAO = new TwitterAccountsDAO();
-	let twitterAccountInfo = await TwitterAccountDAO.getTwitterAccountByPromotion(promotion);
+	const TwitterAccountDAO = new TwitterAccountsDAO();
+	const twitterAccountInfo = await TwitterAccountDAO.getTwitterAccountByPromotion(promotion);
 	TwitterAccountDAO.cleanup()
-	let promotionsDAO = new PromotionsDAO()
-	let promotionInfo = await promotionsDAO.getRandomTweet(promotion);
+	const promotionsDAO = new PromotionsDAO()
+	const promotionInfo = await promotionsDAO.getRandomTweet(promotion);
 	promotionsDAO.cleanup()
-	let twitterAccount = new TwitterPromoter(twitterAccountInfo.username, twitterAccountInfo.password)
+	const twitterAccount = new TwitterPromoter(twitterAccountInfo.username, twitterAccountInfo.password)
 	twitterAccount
 		.tweet(promotionInfo.post, promotionInfo.image)
 		.catch((e) => {
